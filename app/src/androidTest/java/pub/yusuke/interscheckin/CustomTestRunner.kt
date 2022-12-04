d86153afc4fe8a55af50f24e017b3148ce22e277
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2022-12-04 18:12:48 +0900

    Add MockK for testing

---
+++ app/src/androidTest/java/pub/yusuke/interscheckin/CustomTestRunner.kt
@@ -0,0 +1,22 @@
+package pub.yusuke.interscheckin
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+/**
+ * Test 用のランナー
+ * build.gradle (:app) で参照されているが、Kotlin ソース内では参照されていないため
+ * 意図的に unused の警告を Supress している
+ */
+@Suppress("unused")
+class CustomTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}

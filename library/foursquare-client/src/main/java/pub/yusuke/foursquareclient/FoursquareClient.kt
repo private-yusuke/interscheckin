24613e9e4507972343a4b2aa18f6a904d1807037
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2022-12-31 14:14:24 +0900

    Add documentation comments in `FoursquareClient.kt`

--- library/foursquare-client/src/main/java/pub/yusuke/foursquareclient/FoursquareClient.kt
+++ library/foursquare-client/src/main/java/pub/yusuke/foursquareclient/FoursquareClient.kt
@@ -5,6 +5,9 @@ import pub.yusuke.foursquareclient.models.LatAndLong
 import pub.yusuke.foursquareclient.models.Venue
 
 interface FoursquareClient {
+    /**
+     * This function is unused because [searchPlacesNearby] is preferred by me.
+     */
     suspend fun searchPlaces(
         ll: LatAndLong? = null,
         query: String? = null,
@@ -14,6 +17,10 @@ interface FoursquareClient {
         sort: String? = null
     ): List<Venue>
 
+    /**
+     * Retrieves the Venues around the given position [ll].
+     * Calls an V3 API endpoint in the implementation.
+     */
     suspend fun searchPlacesNearby(
         ll: LatAndLong,
         hacc: Double? = null,
@@ -21,6 +28,10 @@ interface FoursquareClient {
         limit: Int? = null
     ): List<Venue>
 
+    /**
+     * This function is unused because of v2 API endpoints are not available.
+     * @see FoursquareClientImpl.searchVenues
+     */
     suspend fun searchVenues(
         ll: LatAndLong? = null,
         near: String? = null,
@@ -30,6 +41,9 @@ interface FoursquareClient {
         limit: Int? = null
     ): List<Venue>
 
+    /**
+     * This function is unused because [searchPlacesNearby] is preferred by me.
+     */
     suspend fun getAutocompleteResults(
         ll: LatAndLong? = null,
         query: String? = null,
@@ -38,6 +52,12 @@ interface FoursquareClient {
         sessionToken: String
     ): List<Venue>
 
+    /**
+     * Creates a new check-in at the Venue with [venueId].
+     * Calls an V2 API endpoint in the implementation.
+     *
+     * @param shout An comment for the check-in.
+     */
     suspend fun createCheckin(
         venueId: String,
         shout: String? = null,

1e428700d45ab616dde9db444de0278f7b9df19d
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2023-01-22 18:04:46 +0900

    Insert `VisitedVenue`s by loading Checkin histories in HistoriesScreen

--- app/src/main/java/pub/yusuke/interscheckin/ui/histories/HistoriesPaging.kt
+++ app/src/main/java/pub/yusuke/interscheckin/ui/histories/HistoriesPaging.kt
@@ -1,25 +1,33 @@
 package pub.yusuke.interscheckin.ui.histories
 
+import co.anbora.labs.spatia.geometry.Point
 import pub.yusuke.foursquareclient.models.Checkin
 import pub.yusuke.foursquareclient.models.url
 import pub.yusuke.interscheckin.repositories.foursquarecheckins.FoursquareCheckinsRepository
+import pub.yusuke.interscheckin.repositories.visitedvenues.VisitedVenue
+import pub.yusuke.interscheckin.repositories.visitedvenues.VisitedVenueDao
 import javax.inject.Inject
 
 class HistoriesPaging @Inject constructor(
-    private val foursquareCheckinsRepository: FoursquareCheckinsRepository
+    private val foursquareCheckinsRepository: FoursquareCheckinsRepository,
+    private val visitedVenueDao: VisitedVenueDao
 ) : HistoriesContract.Paging {
     override suspend fun load(
         userId: Long?,
         beforeTimestamp: Long?,
         offset: Long?,
         perPage: Long
-    ): List<HistoriesContract.Checkin> =
-        foursquareCheckinsRepository.getCheckins(
+    ): List<HistoriesContract.Checkin> {
+        val checkins = foursquareCheckinsRepository.getCheckins(
             userId = userId,
             offset = offset,
             beforeTimestamp = beforeTimestamp,
             limit = perPage
-        ).translateToCheckins()
+        )
+        visitedVenueDao.insertVisitedVenues(checkins.translateToVisitedVenues())
+
+        return checkins.translateToCheckins()
+    }
 
     private fun List<Checkin>.translateToCheckins(): List<HistoriesContract.Checkin> =
         this.map { it.translateToCheckin() }
@@ -37,7 +45,7 @@ class HistoriesPaging @Inject constructor(
         HistoriesContract.Checkin.Venue(
             id = this.id,
             name = this.name,
-            address = this.location?.let {
+            address = this.location.let {
                 listOfNotNull(it.state, it.city).joinToString(" ")
             },
             categoriesString = this.categories.joinToString(", ") { it.name },
@@ -48,5 +56,21 @@ class HistoriesPaging @Inject constructor(
         HistoriesContract.Checkin.Venue.Icon(
             name = this.name,
             url = this.icon.url()
+        )
+
+    private fun List<Checkin>.translateToVisitedVenues(): List<VisitedVenue> =
+        map { it.translateToVisitedVenue() }
+
+    private fun Checkin.translateToVisitedVenue(): VisitedVenue =
+        VisitedVenue(
+            id = this.venue.id,
+            name = this.venue.name,
+            categoriesString = this.venue.categories.joinToString(", ") { it.name },
+            location = Point(
+                this.venue.location.lat,
+                this.venue.location.lng
+            ),
+            iconName = this.venue.categories.firstOrNull()?.name,
+            iconUrl = this.venue.categories.firstOrNull()?.icon?.url()
         )
 }

a30be783b9a40b20db45d30f2869782e0ca84712
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2023-01-22 17:48:56 +0900

    Add `VisitedVenue`

---
+++ app/src/main/java/pub/yusuke/interscheckin/AppDatabase.kt
@@ -0,0 +1,20 @@
+package pub.yusuke.interscheckin
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import co.anbora.labs.spatia.geometry.GeometryConverters
+import pub.yusuke.interscheckin.repositories.visitedvenues.VisitedVenue
+import pub.yusuke.interscheckin.repositories.visitedvenues.VisitedVenueDao
+
+@Database(
+    entities = [
+        VisitedVenue::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(GeometryConverters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun visitedVenueDao(): VisitedVenueDao
+}

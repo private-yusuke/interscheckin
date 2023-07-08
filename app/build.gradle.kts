8967f560847dee7c73ab50b7a811a88e113e71fc
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2023-07-08 15:26:31 +0900

    Add dependency `androidx.compose.material3:material3`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.core)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.gms.play.services.location)
